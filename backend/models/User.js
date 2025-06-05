174424ba827075b1c902f6f3383d6130f678713b
Prajakta <varuteprajakta1@gmail.com>
2025-06-05 11:09:12 +0530

    user.js error removed

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
   enrolledCourses: [
     {
         type: mongoose.Schema.Types.ObjectId,
-        ref: Course
+        ref: 'Course'
     }
   ]
 },{timestamps: true});
